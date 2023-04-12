f1ad1f363aeb1e3f2697627772303ca679c672dd
Chappidi Vamsidhar Reddy <vamsidhar242@gmail.com>
2023-04-12 20:15:08 +0530

    Forgot password screen changes.

---
+++ app/src/main/java/com/capps/mycart/PasswordReset.kt
@@ -0,0 +1,11 @@
+package com.capps.mycart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PasswordReset : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_reset)
+    }
+}
